9c7b9eb344e0b7bd005d66b695682e7ddbe37e21
0zzysome <99025190+0zzysome@users.noreply.github.com>
2023-09-21 10:14:45 +0200

    movement and camera
    start of project with the camera and movement.

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+     float xRotation;
+     float yRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //rotate camera
+        transform.rotation = Quaternion.Euler(xRotation, yRotation,0);
+        
+        // makes shure the orientation is the correct rotation in Y 
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+    public float airDrag;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMult;
+    bool readyToJump;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool isGrounded;
+    
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+
+
+
+    Vector3 direction;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+        rb.freezeRotation = true;
+        ResetJump();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //checks if player is grounded
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        
+        ReadInput();
+        ChangeDrag();
+    }
+    private void FixedUpdate()
+    {
+        movePlayer();
+    }
+    void ReadInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //jump handler 
+        if (Input.GetButton("Jump") && readyToJump && isGrounded) 
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    void movePlayer() 
+    {
+        //makes it so you walk in the direction you are looking
+        direction = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+
+        if(isGrounded)
+        {
+            rb.AddForce(direction.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(direction.normalized * moveSpeed * 10f * airMult, ForceMode.Force);
+        }
+        
+        
+    }
+    private void ChangeDrag()
+    {
+        if (isGrounded) 
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
+    }
+    private void SpeedControll() 
+    {
+        Vector3 flatVel = new Vector3 (rb.velocity.x, 0f, rb.velocity.z);
+        //is the player faster than the movement speed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            //creates the limited velocity, aka max speed
+            Vector3 limit = flatVel.normalized * moveSpeed;
+            //applies it to the rigidbody
+            rb.velocity = new Vector3(limit.x, rb.velocity.y, limit.z);
+        }
+    }
+    private void Jump()
+    {
+        // resets y velocity to make consistent jumps.
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
